abe689b40f255cecb62b3b03e6937144a1d76f44
Женёк <muratov_yevgeniy@vk.com>
2025-05-21 16:06:25 +0300

    final commit

--- src/main/java/com/example/hogwarts/repository/FacultyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hogwarts.repository;
-
-import com.example.hogwarts.model.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository<Faculty, Long> {
-}
--- src/main/java/com/example/hogwarts/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hogwarts.repository;
-
-import com.example.hogwarts.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
