cddeaaf16fe5e0ae25b46cf428b737560e213adf
Женёк <muratov_yevgeniy@vk.com>
2025-05-21 16:11:02 +0300

    final commit

--- src/main/java/com/example/hogwarts/model/Student.java
+++ src/main/java/com/example/hogwarts/model/Student.java
@@ -1,1 +1,38 @@
 
+package com.example.hogwarts.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
