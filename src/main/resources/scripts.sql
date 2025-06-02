274eee83d68f865067f9b8d82e4b11b8cb2080de
Женёк <muratov_yevgeniy@vk.com>
2025-06-02 15:03:07 +0300

    final commit

---
+++ src/main/resources/scripts.sql
@@ -0,0 +1,10 @@
+SELECT * FROM student WHERE age BETWEEN 10 AND 20;
+
+SELECT name FROM student;
+
+SELECT * FROM student WHERE LOWER(name) LIKE '%о%';
+
+SELECT * FROM student WHERE age < id;
+
+
+SELECT * FROM student ORDER BY age;
