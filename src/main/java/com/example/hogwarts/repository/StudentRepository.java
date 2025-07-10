5beb234da4257c0e9c49910f7cca88a84635c4d4
Женёк <muratov_yevgeniy@vk.com>
2025-07-10 16:04:06 +0300

    final request

--- src/main/java/com/example/hogwarts/repository/StudentRepository.java
+++ src/main/java/com/example/hogwarts/repository/StudentRepository.java
@@ -2,18 +2,9 @@ package com.example.hogwarts.repository;
 
 import com.example.hogwarts.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
-    @Query("SELECT COUNT(s) FROM Student s")
-    int getStudentCount();
-
-    @Query("SELECT AVG(s.age) FROM Student s")
-    double getAverageAge();
-
-    @Query("SELECT s FROM Student s ORDER BY s.id DESC LIMIT 5")
-    List<Student> getLastFiveStudents(); // HQL — работает с сущностями
+    List<Student> findByAgeBetween(int min, int max);
 }
