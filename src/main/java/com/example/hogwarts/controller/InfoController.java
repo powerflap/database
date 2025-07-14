71617d4519450290501107be6176f4bb646a9c8b
Женёк <muratov_yevgeniy@vk.com>
2025-07-14 12:22:34 +0300

    исправлена вложенность

--- src/main/java/com/example/hogwarts/controller/InfoController.java
+++ src/main/java/com/example/hogwarts/controller/InfoController.java
@@ -1,17 +1,20 @@
 package com.example.hogwarts.controller;
 
+import com.example.hogwarts.service.InfoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.IntStream;
 
 @RestController
 public class InfoController {
 
+    private final InfoService infoService;
+
+    public InfoController(InfoService infoService) {
+        this.infoService = infoService;
+    }
+
     @GetMapping("/parallel-sum")
     public int getParallelSum() {
-        return IntStream.rangeClosed(1, 1_000_000)
-                .parallel()
-                .reduce(0, Integer::sum);
+        return infoService.calculateParallelSum();
     }
 }
